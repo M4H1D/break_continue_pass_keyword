a1148f04daa7b1d8bea6a102d04e86c9a0dacdc7
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-03-28 23:47:58 +0600

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,3 +1,12 @@
+'''eikhne 'pass' keyword ta use kora hoise.
+eita amneo onk ghapla r important. 
+pass er kahini hoilo ai keyword use korle 
+oita soho baki sob pass korbe.aijonno 
+five likha output e dekha jai.karon se 5 nibe r 5 er 
+nilei five print hobe.5 newar jonnoi pass keyword newa hoisilo.
+eita maximum khetre boro kono error dhora porle oi error nia jate kaj
+kora jai tai use hoi..error handling e besi use hoi.'''
+
 for i in range(1,11):
 	if i==5:
 		pass 
